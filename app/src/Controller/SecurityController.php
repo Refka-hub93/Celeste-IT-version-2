8e303db691309898d20f7a42eb1947915e4c5a23
Refka-hub93 <missaouirefka@outlook.fr>
2025-07-10 15:49:54 +0200

    resetPassword

--- app/src/Controller/SecurityController.php
+++ app/src/Controller/SecurityController.php
@@ -2,9 +2,18 @@
 
 namespace App\Controller;
 
+use App\Form\PasswordForgottenForm;
+use App\Form\ResetPasswordForm;
+use App\Repository\UsersRepository;
+use App\Service\EmailService;
+use App\Service\JWTService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -28,5 +37,120 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    #[Route(path: '/resetPassword', name: 'app_reset_password')]
+    public function resetPassword( Request $request, UsersRepository $usersRepository,
+    JWTService $jwt, EmailService $emailService)  : Response{
+         
+         $form = $this->createForm(ResetPasswordForm::class);
+            // Handle the form 
+            // je reccupére le contenu du form 
+        $form->handleRequest($request); 
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $usersRepository->findOneByEmail($form->get('email')->getData());
+
+            // Si l'utilisateur existe, on génère un token     
+
+            // If the user does not exist, we do not reveal it  
+            if ($user) {
+                 // Génération de token
+
+            $header = [
+                'typ' => 'JWT',
+                'alg' => 'HS256'
+            ];
+
+            $payload = [
+                'user_id' => $user->getId(),
+            ];
+
+            // On génère le token
+
+            $token = $jwt->generate($header, $payload, $this->getParameter('app.jwtsecret'));
+
+            
+                // On génère l'URL vers reset_password
+                $url = $this->generateUrl('reset_password', ['token' => $token], UrlGeneratorInterface::ABSOLUTE_URL);
+
+
+            $emailService->send(
+                'celeste-it@celest-it.fr',                   // from
+                $user->getEmail(),                           // to
+                'Récupération de mot de passe',                 // subject
+                'reset-pass',                                   
+                compact('user', 'url') 
+            );
+            
+      
+
+
+            // On redirige l'utilisateur vers la page de connexion avec un message de succès
+                $this->addFlash('success', 'Email envoyé avec succès');
+                return $this->redirectToRoute('app_login');
+
+            }
+
+
+            // Si l'utilisateur n'existe pas, on ne révèle pas son existence
+            // On redirige l'utilisateur vers la page de connexion avec un message d'erreur
+     $this->addFlash('error', 'un problème est survenu');
+                
+                return $this->redirectToRoute('app_login');
+        }
+
+            // vue twig
+        return $this->render('security/reset_password.html.twig', [
+            'requestPassForm' => $form->createView()
+        ]);
+    
+
+}
+
+// requirements: ['token' => '.+'] indique à Symfony d'accepter n'importe quel caractère, y compris les . et _ dans {token}.
+    #[Route('/resetPassword/{token}', name: 'reset_password', requirements: ['token' => '.+'])]
+    public function resetPasswordToken(
+        $token,
+        JWTService $jwt,
+        UsersRepository $usersRepository,
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface $em
+    ): Response
+    {
+        
+        // On vérifie si le token est valide (cohérent, pas expiré et signature correcte)
+        if($jwt->isValid($token) && !$jwt->isExpired($token) && $jwt->check($token, $this->getParameter('app.jwtsecret'))){
+            // Le token est valide
+            // On récupère les données (payload)
+            $payload = $jwt->getPayload($token);
+            
+            
+            // On récupère le user
+            $user = $usersRepository->find($payload['user_id']);
+
+            if($user){
+                $form = $this->createForm(PasswordForgottenForm::class);
+
+                $form->handleRequest($request);
+
+                if($form->isSubmitted() && $form->isValid()){
+                    $user->setPassword(
+                        $passwordHasher->hashPassword($user, $form->get('password')->getData())
+                    );
+
+                    $em->flush();
+
+                    $this->addFlash('success', 'Mot de passe changé avec succès');
+                    return $this->redirectToRoute('app_login');
+                }
+                return $this->render('security/reset_password.html.twig', [
+                    'passForm' => $form->createView()
+                ]);
+            }
+        }
+        $this->addFlash('danger', 'Le token est invalide ou a expiré');
+        return $this->redirectToRoute('app_login');
     }
 }
--- app/src/Controller/SubscribedController.php
+++ app/src/Controller/SubscribedController.php
@@ -1,13 +1,10 @@
 <?php
 
 namespace App\Controller;
-
-use App\Entity\Tables; // Ton entité Tables
+ 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+ 
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
 
 final class SubscribedController extends AbstractController
 {
@@ -19,42 +16,4 @@ final class SubscribedController extends AbstractController
         return $this->render('profil/subscribed.html.twig');
     }
 
-    // // Création du tableau
-    // #[Route('/table', name: 'app_table', methods: ['POST'])]
-    // public function createTable(Request $request, EntityManagerInterface $entityManager)
-    // {
-    //     try {
-    //         // Récupérer les données du formulaire (titre)
-    //         $title = $request->request->get('title');
-
-    //         if (!$title) {
-    //             throw new \Exception('Le titre est requis');
-    //         }
-
-    //         // Créer un nouveau tableau
-    //         $table = new Tables();
-    //         $table->setTableTitle($title);  // Assurez-vous que la méthode setTableTitle existe dans ton entité
-
-    //         // Sauvegarder dans la base de données
-    //         $entityManager->persist($table);
-    //         $entityManager->flush();
-
-    //         // Récupérer l'ID du tableau nouvellement créé
-    //         $tableId = $table->getId();
-
-    //         // Retourner une réponse JSON avec l'URL de redirection
-    //         return new Response(
-    //             json_encode(['success' => true, 'redirect_url' => $this->generateUrl('table', ['id' => $tableId])]),
-    //             Response::HTTP_OK,
-    //             ['Content-Type' => 'application/json']
-    //         );
-    //     } catch (\Exception $e) {
-    //         // En cas d'erreur, retourner un message d'erreur en JSON
-    //         return new Response(
-    //             json_encode(['success' => false, 'error' => $e->getMessage()]),
-    //             Response::HTTP_INTERNAL_SERVER_ERROR,
-    //             ['Content-Type' => 'application/json']
-    //         );
-    //     }
-    // }
 }
